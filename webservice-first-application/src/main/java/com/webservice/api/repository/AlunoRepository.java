6dcbee2dc39fb1185f84225fb668b819bd00e1f8
Daniel Soares Ferreira <39291799+danielsf2@users.noreply.github.com>
2021-01-22 23:51:17 -0300

    Delete webservice-first-application directory

--- webservice-first-application/src/main/java/com/webservice/api/repository/AlunoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.webservice.api.repository;
-
-import com.webservice.api.models.Aluno;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AlunoRepository extends CrudRepository<Aluno, Integer> { }
--- webservice-first-application/src/main/java/com/webservice/api/repository/TurmaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.webservice.api.repository;
-
-import com.webservice.api.models.Turma;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TurmaRepository extends CrudRepository<Turma, Integer> {}
